97301af4be4463cde3cdeab12febc47dda567eef
Tanuja Suryawanshi <tanujasuryawanshi07@gmail.com>
2026-08-26 20:59:52 +0530

    Correct file path for dataset loading

--- src/02_eda_profiling.py
+++ src/02_eda_profiling.py
@@ -5,7 +5,7 @@ from ydata_profiling import ProfileReport
 # 1. Load Dataset
 # ==========================================
 
-file_path = "data/E Commerce Dataset-E Comm.csv"
+file_path = "Data/E Commerce Dataset-E Comm.csv"
 
 df = pd.read_csv(file_path)
 
