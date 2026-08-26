ebd458b834e6e9d9bccce905c6d20fa290a57f5d
Tanuja Suryawanshi <tanujasuryawanshi07@gmail.com>
2026-08-26 21:02:55 +0530

    Fix file path for dataset loading

--- src/04_ml_modeling.py
+++ src/04_ml_modeling.py
@@ -6,7 +6,7 @@ from sklearn.feature_selection import VarianceThreshold
 # 1. Load Dataset
 # ==========================================
 
-file_path = "data/E Commerce Dataset-E Comm.csv"
+file_path = "Data/E Commerce Dataset-E Comm.csv"
 
 df = pd.read_csv(file_path)
 
